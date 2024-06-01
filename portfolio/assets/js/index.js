e1b3f3cdd14e596679131cab4ffd667fb7192e8e
Sai Charanteja <107063715+UsaSai99charanteja@users.noreply.github.com>
2024-06-01 01:36:24 -0500

    Add files via upload

---
+++ portfolio/assets/js/index.js
@@ -0,0 +1,7 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://9c079lun24.execute-api.us-east-1.amazonaws.com/viewcount")
+    let data = await response.json()
+    counter.innerHTML = ` Views Report: ${data}`;
+}
+updateCounter();
